e0f0b01cc7a48a0ea7a8a1014b5858f6388a8770
jstyro <apurv.khadamkar07@gmail.com>
2019-07-02 02:15:44 +0530

    sample component init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SampleComponentComponent } from './sample-component/sample-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SampleComponentComponent
   ],
   imports: [
     BrowserModule
